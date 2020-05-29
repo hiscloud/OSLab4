d290f80c159604688a64cb6e425f6cab03daf7bd
hiscloud <49000237+hiscloud@users.noreply.github.com>
2020-05-28 20:37:41 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 using namespace std;
-
+// todoist: commandline filename, wait
 
 struct PCB{
     char process_name[16];
@@ -17,7 +17,77 @@ struct PCB{
     int limit_register;
     char priority;
 };
-void write(PCB p,const chat* outFilename){
+void read(PCB * process,string outfileName,int size){
+    FILE *fptr;
+    const char *modifyingFile=outfileName.c_str();
+    if((fptr=fopen(modifyingFile,"rb"))==NULL)
+    {printf("Error! opening file");
+     exit(1);
+    }
+    for(int i=0;i<size;i++)
+    { fread(&process[i].process_name,sizeof(char)*16,1,fptr);
+      fread(&process[i].process_id,32/8,1,fptr);
+      fread(&process[i].activity_status,sizeof(char),1,fptr);
+      fread(&process[i].CPU_burst_time,32/8,1,fptr);
+      fread(&process[i].base_register,32/8,1,fptr);
+      fread(&process[i].limit_register,64/8,1,fptr);
+      fread(&process[i].priority,sizeof(char),1,fptr);
+    }
+    fclose(fptr);
+}
+
+void write(PCB * process,string outfileName,int size){
+    FILE *fptr;
+    const char *modifyingFile=outfileName.c_str();
+    if((fptr=fopen(modifyingFile,"wb"))==NULL)
+    {printf("Error! opening file");
+     exit(1);
+    }
+    for(int i=0;i<size;i++)
+    { fwrite(&process[i].process_name,sizeof(char)*16,1,fptr);
+      fwrite(&process[i].process_id,32/8,1,fptr);
+      fwrite(&process[i].activity_status,sizeof(char),1,fptr);
+      fwrite(&process[i].CPU_burst_time,32/8,1,fptr);
+      fwrite(&process[i].base_register,32/8,1,fptr);
+      fwrite(&process[i].limit_register,64/8,1,fptr);
+      fwrite(&process[i].priority,sizeof(char),1,fptr);
+    }
+    fclose(fptr);
+}
+bool allClear (PCB* process, int size)
+{   
+    for (int i=0;i<size;i++)
+        if ((int)(process[i].activity_status)==1)
+            return false;
+    cout<<"all clear"<<endl;
+    return true;
+}
+
+void execute(PCB * process, string outfileName, int size)
+{   int i;
+    int loop=0;
+    while(!allClear(process,size))
+    {   i=0;
+        
+        for (int t=1;t<=30;t++)
+        {   
+            while(process[i].CPU_burst_time==0)
+                i++;
+            if (process[i].CPU_burst_time>0)
+            {   cout<< "Round Robin on process "<<process[i].process_id<<", remaining CPU burst: "<<process[i].CPU_burst_time<<", time="<<t+loop*60<<endl;
+                process[i].CPU_burst_time--;
+                i++;
+            }else
+            {
+                process[i].activity_status=char(0);
+                if(i<size-1)
+                    i++;
+                t--;
+            }
+           
+        }//end of RR for loop
+        loop++;
+     }
     
 }
 
@@ -42,7 +112,7 @@ int main()
         istreambuf_iterator<char>(input), 
         istreambuf_iterator<char>( ),
         ostreambuf_iterator<char>(output));
-    //cout<<input.tellg()<<endl;
+   
     int bitN =input.tellg();
     int size=bitN/38;
 
@@ -50,15 +120,14 @@ int main()
     input.close();
     output.close();
 //end of copying 
-    FILE *fptr;
-   // cout<<size;/////////////
+ /*   FILE *fptr;
+   
     const char *modifyingFile=outfileName.c_str();
     if((fptr=fopen(modifyingFile,"rb"))==NULL)
     {printf("Error! opening file");
      exit(1);
     }
-    cout<<sizeof(char);
-    
+   
     for(int i=0;i<size;i++)
     { fread(&process[i].process_name,sizeof(char)*16,1,fptr);
       fread(&process[i].process_id,32/8,1,fptr);
@@ -68,10 +137,11 @@ int main()
       fread(&process[i].limit_register,64/8,1,fptr);
       fread(&process[i].priority,sizeof(char),1,fptr);
     }
-    fread(&process[0].process_name,sizeof(char)*16,1,fptr);
-    fclose(fptr);
-    // process structs read
-    for (int i=0;i<size;i++){
+   
+    fclose(fptr);*/
+    read(process, outfileName,size);
+    // process structs are read
+     for (int i=0;i<size;i++){
         cout<<process[i].process_name<<"\t";
         cout<<process[i].process_id<<"\t";
         cout<<process[i].CPU_burst_time<<"\t";
@@ -80,5 +150,19 @@ int main()
         cout<<(int)(process[i].priority)<<"\t";
         cout<<(int)(process[i].activity_status)<<endl;
     }
+    write(process,outfileName,size);
+    int counter=0;
+    int memsumBase=0;
+    int memsumLimit=0;
+    for (int i=0;i<size;i++)
+        if ((int)(process[i].activity_status)==1)
+        {   counter++;
+            memsumBase+=process[i].base_register;
+            memsumLimit+=process[i].limit_register;
+        }
+    cout<<"There's "<<counter<<" active processes list in "<<infileName<<endl;
+    cout<<"the total memory allocated by the processes is between "<<memsumBase<<" to "<<memsumLimit<<endl;
+    cout<<char(0)<<char(1)<<endl;
+   // execute(process, outfileName,size); 
     return 0;
 }
